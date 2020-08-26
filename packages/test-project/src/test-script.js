bed539a2e09ff729490740d3c35e1fb8e4d0cb81
Oleksandr Palokha <palokha.oa@gmail.com>
2020-08-26 19:00:12 +0300

    Added video tag creation to test-app

--- packages/test-project/src/test-script.js
+++ packages/test-project/src/test-script.js
@@ -1,8 +1,22 @@
 const { WRTConf } = require('@wrtconf/client');
 
-navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
+function createVideo(id) {
+    const video = document.createElement('video');
+    video.autoplay = true;
+    video.id = id;
+    document.body.append(video);
+    return video;
+}
+
+navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(stream => {
     const conf = new WRTConf('ws://localhost:8080/wrtconf', stream);
 
-    conf.clients$.subscribe(console.log);
-    console.log(conf);
+    conf.clients$.subscribe(clients => clients.forEach(client => {
+        if (client.stream) {
+            const id = 'a' + client.id;
+            const video = document.querySelector(`#${id}`) ||
+                createVideo(id);
+            video.srcObject = client.stream;
+        }
+    }));
 });
